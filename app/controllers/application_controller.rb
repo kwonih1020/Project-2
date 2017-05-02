e0c995dd75f5150a4cb80815cff64c104395515d
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-02 14:06:28 -0400

    finished for css and solved rating system problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, with: :couldnt_find_record
+
+  private
+  def couldnt_find_record
+    redirect_to root_url, notice: "That record doesn't exist!"
+  end
 end
